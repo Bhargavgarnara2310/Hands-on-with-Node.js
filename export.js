1a646923f2a924617f168f0aefeb92208f4f6c84
Bhargav Garnara <bgarnara481@rku.ac.in>
2021-12-31 09:19:57 +0530

    Export Function

---
+++ export.js
@@ -0,0 +1,13 @@
+    var sum = (a,b)=>{
+    return a+b;
+         }
+    
+    var sub = (a,b)=>{
+        return a-b;
+        }
+    
+    var mul = (a,b)=>{
+        return a*b;
+        }
+    module.exports = (sum,sub,mul);
+    console.log("Expoeted File")
